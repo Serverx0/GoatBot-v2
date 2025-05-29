d52d427391da45b84dd84c361774b1a48840bf8a
Serverx0 <razrender@gmail.com>
2025-05-29 23:33:10 +0530

    Update owner.js

--- scripts/cmds/owner.js
+++ scripts/cmds/owner.js
@@ -7,7 +7,7 @@ module.exports = {
 	config: {
 		name: "owner",
 		aliases: ["info"],
-		author: "Hasan",
+		author: "Abhishek Sahu",
 		role: 0,
 		shortDescription: " ",
 		longDescription: "",
@@ -18,15 +18,15 @@ module.exports = {
 	onStart: async function ({ api, event }) {
 		try {
 			const ownerInfo = {
-				name: '𝑯𝑨𝑺𝑨𝑵 𝑴𝑨𝑯𝑴𝑼𝑫',
-				class: '𝑺𝑺𝑪 𝑪𝑨𝑵𝑫𝑰𝑫𝑨𝑻𝑬',
+				name: 'Abhishek Sahu',
+				class: 'LODA LASUN',
 				group: '𝑺𝑪𝑰𝑬𝑵𝑪𝑬',
 				gender: '𝑴𝑨𝑳𝑬',
-				Birthday: '𝟐𝟐-𝟎𝟕-𝟐𝟎𝟎𝟖',
-				religion: '𝑰𝑺𝑳𝑨𝑴',
-				hobby: '𝑭𝒍𝒊𝒓𝒕𝒊𝒏𝒈 😁',
-				Fb: 'https://www.facebook.com/Itz.HaSaN.00',
-				Relationship: '𝑨𝑳𝑾𝑨𝒀𝑺 𝑩𝑬 𝑺𝑰𝑵𝑮𝑳𝑬',
+				Birthday: '13-05-𝟐𝟎𝟎6',
+				religion: 'HINDU',
+				hobby: 'WEB DEVELOPMENT',
+				Fb: 'https://www.facebook.com/profile.php?id=61561050269428',
+				Relationship: 'KYU BATAU TUJHE ?',
 				Height: '5"4'
 			};
 
